e7d27c64a55be49de1f0be527b2a70f789060788
DarkTower2Ds <DarkTower2Ds@users.noreply.github.com>
2017-09-12 12:28:59 -0500

    Basic Setup
    I created the file we'll use for the assignment with an outline of the functions we should need. I've commented in front of each what is said on the assignment sheet.

---
+++ Group Assignment 0/maincpp.cpp
@@ -0,0 +1,51 @@
+/*
+	Hello friends! I created this .cpp file as the base for out assignment,
+	including the #include stuff, and an outline of every function required
+	by the assignment. We should all be able to use GitHub to individually
+	edit certain functions, which we will then push to a final version that
+	we can all use and turn in together.
+										-Alex Andrews
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
+
+//"read the weights from the keyboard and fill the array and update the number of exams"
+void getws(float weit[], int &no_of_exams)
+{
+
+}
+
+//"read from file and fill the double dimensional array"
+void read_scores(int scores[][], int r, int c)
+{
+
+}
+
+/*"this function is to read scores, compute average based on the wieght and save into
+   finalgrade.txt
+   like
+   70	89	77	75.8"
+*/
+void we_avg(int scores[][], int r, int c, float weight[])
+{
+
+}
+
+//"computes average exams and appends to finalgrade.txt"
+void avg_exams(int scores[][], int r, int c)
+{
+	
+}
+
+//(There isn't a description on the sheet, but I think it should be obvious what this function does)
+void print_scores(int scores[][], int r, int c)
+{
+
+}
